49ad5fa8728446cba90f0cac7b986272ba257f64
buistvo <plus100500@gmail.com>
2023-10-04 16:20:33 +0300

    update product endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,6 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product:
+    "https://u0sl31di08.execute-api.eu-north-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
