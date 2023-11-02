d8232dcc9bd9980d0e5934c8cdbf9aedaa413a33
buistvo <plus100500@gmail.com>
2023-11-02 12:39:40 +0300

    use auth token

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -26,21 +26,25 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
     console.log("uploadFile to", url);
 
     // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
+    const response = await axios({
+      method: "GET",
+      url,
+      params: {
+        name: encodeURIComponent(file!.name),
+      },
+    });
+    console.log("File to upload: ", file!.name);
+    console.log("Uploading to: ", response.data);
+    const authToken = localStorage.getItem("authorization_token");
+    const result = await fetch(response.data.signedUrl, {
+      method: "PUT",
+      body: file,
+      headers: {
+        authorization: `Basic ${authToken}`,
+      },
+    });
+    console.log("Result: ", result);
+    setFile(undefined);
   };
   return (
     <Box>
