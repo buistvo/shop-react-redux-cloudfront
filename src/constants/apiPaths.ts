8e3a25ec6bde4cc5ece5a048423068c09f442358
buistvo <plus100500@gmail.com>
2023-11-11 18:08:36 +0300

    fix api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const products_api = import.meta.env.DEV
 
 const cart_api = import.meta.env.DEV
   ? "http://localhost:3003/dev/api"
-  : "https://u0sl31di08.execute-api.eu-north-1.amazonaws.com/dev";
+  : "https://8m1eujh0gf.execute-api.eu-north-1.amazonaws.com/dev/api";
 const API_PATHS = {
   product: products_api,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
