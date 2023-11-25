9ddbc430c61b349a2c5d86a79bd5acbafae1b72c
buistvo <plus100500@gmail.com>
2023-11-25 21:43:22 +0400

    use beanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const products_api = import.meta.env.DEV
 
 const cart_api = import.meta.env.DEV
   ? "http://localhost:3003/dev/api"
-  : "http://buistvo-cart-api-develop.eu-north-1.elasticbeanstalk.com/api";
+  : "https://buistvo-cart-api-develop.eu-north-1.elasticbeanstalk.com/api";
 const API_PATHS = {
   product: products_api,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
